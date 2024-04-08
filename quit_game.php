33e1e2ffd471617a4ce7bf4caf623bff93cb1d75
Jay Patel <104886785+jayPatel029@users.noreply.github.com>
2024-04-08 21:05:03 +0530

    Update quit_game.php

--- quit_game.php
+++ quit_game.php
@@ -16,7 +16,7 @@ $user_id = $_SESSION['user_id'];
 $game_id = mysqli_real_escape_string($conn, $_POST["game_id"]);
 
 // Check if the game ID is associated with the logged-in user
-$check_game_query = "SELECT COUNT(*) AS count FROM UserGames WHERE user_id = ? AND game_id = (SELECT id FROM Games WHERE game_id = ?)";
+$check_game_query = "SELECT COUNT(*) AS count FROM usergames WHERE user_id = ? AND game_id = (SELECT id FROM games WHERE game_id = ?)";
 $stmt_check_game = $conn->prepare($check_game_query);
 $stmt_check_game->bind_param("is", $user_id, $game_id);
 $stmt_check_game->execute();
@@ -29,14 +29,14 @@ if ($row_check_game['count'] == 0) {
 }
 
 // Prepare and execute SQL statement to update user state and deactivate user in UserGames table
-$update_query = "UPDATE UserGames SET player_state = 'folded', is_active = false WHERE user_id = ? AND game_id = (SELECT id FROM Games WHERE game_id = ? AND status = 'waiting')";
+$update_query = "UPDATE usergames SET player_state = 'folded', is_active = false WHERE user_id = ? AND game_id = (SELECT id FROM games WHERE game_id = ? AND status = 'waiting')";
 
 $stmt_update = $conn->prepare($update_query);
 $stmt_update->bind_param("is", $user_id, $game_id);
 
 if ($stmt_update->execute()) {
     // If update is successful, decrement num_players in GameInfo table
-    $update_game_info_query = "UPDATE GameInfo SET num_players = num_players - 1 WHERE game_id = ?";
+    $update_game_info_query = "UPDATE gameinfo SET num_players = num_players - 1 WHERE game_id = ?";
     $stmt_update_game_info = $conn->prepare($update_game_info_query);
     $stmt_update_game_info->bind_param("s", $game_id);
     $stmt_update_game_info->execute();
